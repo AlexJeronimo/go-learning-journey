ef6ff5646db86651fef3fa3e69d6a4b13553ac0d
Oleksandr Pidhurskyi <opidhurskyi@gmail.com>
2025-05-29 15:46:59 +0300

    day4 task3 completed

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,5 +12,24 @@ func main() {
 		fmt.Fprintf(w, "Hello, Go Web!")
 	})
 
+	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			name = "guest"
+		}
+		fmt.Fprintf(w, "Hello, %s!\n", name)
+	})
+
+	http.HandleFunc("/api/user", apiUser)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func apiUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	data := struct {
+		Name  string
+		Email string
+	}{Name: "Jane Doe", Email: "jane@example.com"}
+	json.NewEncoder(w).Encode(data)
 }
