b9f93582d768cf20457909ea569b7f09ad3f0741
Oleksandr Pidhurskyi <opidhurskyi@gmail.com>
2025-05-29 15:35:08 +0300

    day4 task 3.1. completed

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, Go Web!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
