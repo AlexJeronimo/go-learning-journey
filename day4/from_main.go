d5ef17e18d3c02717c3f24063c5bf4e217b3545b
Oleksandr Pidhurskyi <opidhurskyi@gmail.com>
2025-05-29 12:59:03 +0300

    day4 task2.3 completed

--- day4/from_main.go
+++ day4/from_main.go
@@ -34,3 +34,54 @@ dir, _ := os.Getwd()
 		fmt.Println(file)
 	}
 */
+
+/*
+u := day4.User{Name: "Alice", Email: "alice@example.com", Age: 30, IsActive: true}
+	data, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	jsonBytes := string(data)
+	fmt.Println(jsonBytes)
+
+	jsonString := `{"name":"Bob","email":"bob@example.com","age":25,"is_active":false}`
+
+	var newUser day4.User
+
+	json.Unmarshal([]byte(jsonString), &newUser)
+
+	fmt.Println(newUser)
+*/
+
+/*
+users := []day4.User{
+		{Name: "Alice", Email: "alice@example.com", Age: 30, IsActive: true},
+		{Name: "Bob", Email: "bob@example.com", Age: 25, IsActive: false},
+		{Name: "John", Email: "john@example.com", Age: 31, IsActive: true},
+	}
+
+	jsonBytes, err := json.MarshalIndent(users, "", " ")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = os.WriteFile("users.json", jsonBytes, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var loadUsers []day4.User
+
+	data, err := os.ReadFile("users.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = json.Unmarshal(data, &loadUsers)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(loadUsers)
+*/
--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"glp/day4"
-)
-
 func main() {
-	u := day4.User{Name: "Alice", Email: "alice@example.com", Age: 30, IsActive: true}
-	data, err := json.Marshal(u)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	jsonBytes := string(data)
-	fmt.Println(jsonBytes)
-
-	jsonString := `{"name":"Bob","email":"bob@example.com","age":25,"is_active":false}`
-
-	var newUser day4.User
-
-	json.Unmarshal([]byte(jsonString), &newUser)
-
-	fmt.Println(newUser)
 
 }
